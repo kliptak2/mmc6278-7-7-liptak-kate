7bcfc113134717042f6cd5c4d5f56602014e247b
Kate Liptak <kate.liptak@gmail.com>
2022-07-01 01:29:23 -0400

    generateQuestion & endGame

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 // Your code here
+
+// Global variables
+var quizDiv = document.getElementById("quiz");
+var timerId = "";
+var currentQuestion = null;
+var startBtn = null;
+var correct = 0;     // Don’t need to keep track of incorrects. Just corrects/questionsArr.length
+var questionIndex = 0;
 var questionsArr = [
     {
         question: "Which of these states doesn't have a 'panhandle'?",
@@ -50,123 +58,105 @@ var questionsArr = [
       },
 ]
 
-// Global variables
-var startBtn = document.createElement("button");
-var quizDiv = document.getElementById("quiz")
-var timerId = ""
-var index = 0
-Question = null
-Correct = 0
-// Don’t need to keep track of incorrects. Just corrects / array length.
-// For (i=0; i<arr.length; i++) {
+//Loop through quiz questions
+var generateQuestion = function (index){
+    if (index >= questionsArr.length){
+        endGame();
+        return;
+    }
+    var question = questionsArr[index];
+    quizHTML(question);
+    startCountdown(30);
+}
 
-// }
+function startGame() {
+    // try to find prev score
+    // if yes display
+    // make button
+    // button will start game
+    // Add button (and maybe previous score) to screen
+    //     element.replaceChildren(prevScore, button)
+        //create empty <p> tag to maybe put previous score in
+        //Get “previous-score” from localStorage
+        //if you get a real value for “previous-score” put it in <p> tag
+        //Create your start button document.createElement(“button”)
+        //startBtn.onclick = handleNewQuestion
+        //quiz.replaceChildren(prevScore, button)
+}
+    
+function handleNewQuestion() {
+    //Check if game is over before trying to get next question
+    //if index === 0
+//set previousScore <p> tag innerHtml = “” even if it already was empty
+    //quiz.removeChild(startBtn)
+    //is our index >= length of the questions arr
+//if the index is not possible, end the game gameOver()
+//     question = questionsArr[index]
+//     setQuizHtml()
+// createCountdown()
+}
+    
+function quizHtml() {
+    //     <div id=”quiz”>
+    //         <p>{question.question}</p>
+    // <div id=”innerDiv”>
+    // <button value={option1}>Option 1</button>
+    // <button value={option2}>Option 2</button>
+    // <button value={option3}>Option 3</button>
+    // <button value={option4}>Option 4</button>
+    // </div>
+    // <p id=”timer”>{time}</p>
+    // </div>
+    //document.createElement(“p”) with question.question as innerHTML
+    //document.createElement(“div) with id of “innerDiv”
+    //Loop over options: for each option let btn = document.createElement(“button”) 
+    //btn.value = <option>           (this is hidden)
+    //btn.innerHtml = <option>   (this will be seen)
+    //btn.onclick = handleAnswer
+    //innerDiv.append(btn)
+    //Once all buttons are created and appended to innerDiv, document.createElement(“p”) with id=”timer”
+    //quiz.replaceChildren(question, innerDiv, timer)
+}
+    
+function startCountdown() {
+    //let secondsRemaining = 30
+    //Get the <p> element we just created that will hold the current timer value and set it to 30
+    //timerId = setInterval(()=>{
+        //secondsRemaining- - 
+        //timer.innerHtml = secondsRemaining
+        //Check if secondsRemaining === 0, if yes handleTimerRunout()
+    // }, 1000)
+}
+    
+function timerRunout() {
+    //Clear the current interval
+    //update the global question index
+    //handleNewQuestion()
+}
+    
+function handleAnswer(e) {
+    //get e.target.value
+    //compare e.target.value to question.answer
+    //update correct if the user picked the right answer
+    //Clear the interval
+    //update the global question index
+    //handleNewQuestion()
+}
+
+function endGame() {
+    var score = Math.round((correct/questionsArr.length) * 100);
+    localStorage.setItem(“previous-score”, score);
+    correct = 0;
+    questionIndex = 0;
+    startGame();
+};
+
+window.onload = startGame();
 
 /*
 PSEUDOCODE
-
 *Can’t use loops for this assignment bc can’t stop them w/o using alert, confirm, prompt
 *You should be able to go to the next question with a click or a timeout. So on Page you'll cycle through the data
 *setInterval => do something every set amount of time until the interval is canceled
 *setTimeout=> wait a set amount of time then do something once
-
-
-//Global variables
-Div = the quiz div
-timerId = “”
-Index = 0
-Question = null
-startBtn = null
-Correct = 0
-
-
-//Simulate loop
-Global variable for index
-Index = 0
-currentQuestion = null
-Function for timer runout: index++
-Function for user clicked on an answer: index++ -> handleNewQuestion()
-
-startGame = () => {
-try to find prev score
-if yes display
-make button
-button will start game
-Add button (and maybe previous score) to screen
-	element.replaceChildren(prevScore, button)
-	//create empty <p> tag to maybe put previous score in
-	//Get “previous-score” from localStorage
-	//if you get a real value for “previous-score” put it in <p> tag
-	//Create your start button document.createElement(“button”)
-	//startBtn.onclick = handleNewQuestion
-	//quiz.replaceChildren(prevScore, button)
-}
-
-handleNewQuestion = () => {
-	//Check if game is over before trying to get next question
-	//if index === 0
-//set previousScore <p> tag innerHtml = “” even if it already was empty
-	//quiz.removeChild(startBtn)
-	//is our index >= length of the questions arr
-//if the index is not possible, end the game gameOver()
-	question = questionsArr[index]
-	setQuizHtml()
-createCountdown()
-}
-
-setQuizHtml = () => {
-	<div id=”quiz”>
-		<p>{question.question}</p>
-<div id=”innerDiv”>
-<button value={option1}>Option 1</button>
-<button value={option2}>Option 2</button>
-<button value={option3}>Option 3</button>
-<button value={option4}>Option 4</button>
-</div>
-<p id=”timer”>{time}</p>
-</div>
-//document.createElement(“p”) with question.question as innerHTML
-//document.createElement(“div) with id of “innerDiv”
-//Loop over options: for each option let btn = document.createElement(“button”) 
-//btn.value = <option>           (this is hidden)
-//btn.innerHtml = <option>   (this will be seen)
-//btn.onclick = handleAnswer
-//innerDiv.append(btn)
-//Once all buttons are created and appended to innerDiv, document.createElement(“p”) with id=”timer”
-//quiz.replaceChildren(question, innerDiv, timer)
-}
-
-createCountdown = () => {
-	//let secondsRemaining = 30
-	//Get the <p> element we just created that will hold the current timer value and set it to 30
-	//timerId = setInterval(()=>{
-		//secondsRemaining- - 
-		//timer.innerHtml = secondsRemaining
-		//Check if secondsRemaining === 0, if yes handleTimerRunout()
-	}, 1000)
-}
-
-handleTimerRunout = () => {
-	//Clear the current interval
-	//update the global question index
-	//handleNewQuestion()
-}
-
-handleAnswer = (e) => {
-	//get e.target.value
-	//compare e.target.value to question.answer
-	//update correct if the user picked the right answer
-	//Clear the interval
-	//update the global question index
-	//handleNewQuestion()
-}
-
-gameOver = () => {
-//Calculate score (correct/questionsArr.length *100)
-//Set score in localStorage as “previous-score”
-//reset correct and index to 0
-//startGame()
-}
-
-
 */
